6afec9f57055954bd3d031c2713538e17f2de2a0
Karen <karen.chen@hiero7.com>
2019-09-23 11:14:16 +0800

    dataTable UI adjusted

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -3,14 +3,15 @@
         v-data-table.elevation-1(:headers="headers" :items="items" :loading="loading" :search="searchText" :pagination.sync="pagination"  hide-actions)
             template(slot="items" slot-scope="props")
                 slot(name="items" :props="props" :index="rowIndex(props.index)")
-                
-        v-layout.px-2(row align-center)
-            v-flex.text-xs-left.py-3(xs4)
-                slot(name="actions-left")
-            v-layout(row align-center)
-                v-layout(row align-center)
-                    small Rows per page:
-                    v-select.px-3.py-3(:items="page" v-model="perPage" hide-details)
+
+        v-flex.px-2        
+            v-layout.px-2(row align-center)
+                v-flex.text-xs-left
+                    v-layout(row align-center)
+                        small Rows per page:
+                        v-select.px-3.py-3(:items="page" v-model="perPage" hide-details)
+                //- v-flex.text-xs-left.py-3(xs4)
+                //-     slot(name="actions-left")
                 v-flex.text-xs-right.py-3(xs8)
                     v-pagination(v-model="pagination.page" :length="pages" :total-visible="7")
 </template>
