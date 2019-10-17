cf08a9b23ecee0cea6205b019233c3acd0c2d39e
Karen <karen.chen@hiero7.com>
2019-10-17 17:19:27 +0800

    error message mapping

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,8 @@
 const errorMapping = {
     1001: "Invalid email or password",
-    1002: "Invalid email or password"
-}
+    1002: "Invalid email or password",
+    5005: "Please contact iRoute admin.",
+    3012: "Please wait for a while to scan."
+};
 
 export default errorMapping;
